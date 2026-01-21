2c8bde78a10158adb14c8f66f1e6ebdf0f47f986
Denisha-Madhura <denisha.madhura2007@gmail.com>
2026-01-21 22:46:00 +0530

    eight step: execution of all basic commands

--- app/main.go
+++ app/main.go
@@ -5,39 +5,208 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
+type RedirectionType int
+
+const (
+	TokenRedirectOut    RedirectionType = iota // >
+	TokenRedirectAppend                        // >>
+	TokenRedirectIn                            // <
+	TokenRedirect2                             // 2>
+)
+
+type Redirection struct {
+	Type     RedirectionType
+	Filename string
+}
+
+type Command struct {
+	Args         []string
+	Redirections []Redirection
+}
+
+type Shell struct {
+	// You can add shell state here (like environment variables)
+}
+
+var Builtins = map[string]bool{
+	"exit": true, "echo": true, "type": true, "cd": true,
+}
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
+	shell := &Shell{}
+	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		fmt.Print("$ ")
-
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
-			os.Exit(1)
+			break
 		}
-		command = strings.TrimSpace(command)
-		switch {
-		case command == "exit":
-			os.Exit(0)
 
-		case strings.HasPrefix(command, "echo"):
-			fmt.Println(strings.TrimPrefix(command, "echo "))
-		case strings.HasPrefix(command, "type"):
-			if strings.HasSuffix(command, "exit") || strings.HasSuffix(command, "echo") || strings.HasSuffix(command, "type") {
-				fmt.Println(strings.TrimPrefix(command, "type ") + " is a shell builtin")
-			} else if execPath, err := exec.LookPath(strings.TrimPrefix(command, "type ")); err == nil {
-				fmt.Println(strings.TrimPrefix(command, "type ") + " is " + execPath)
-			} else {
-				fmt.Println(strings.TrimPrefix(command, "type ") + ": not found")
+		input = strings.TrimSpace(input)
+		if input == "" {
+			continue
+		}
+
+		parts := strings.Fields(input)
+		cmd := &Command{
+			Args: parts,
+		}
+
+		if cmd.Args[0] == "exit" {
+			os.Exit(0)
+		}
+
+		cmd.Execute(shell)
+	}
+}
+
+func (c *Command) Execute(shell *Shell) int {
+	if len(c.Args) == 0 {
+		return 0
+	}
+
+	cmdName := c.Args[0]
+
+	if Builtins[cmdName] {
+		return c.executeBuiltin(shell)
+	}
+
+	return c.executeExternal()
+}
+
+func (c *Command) executeBuiltin(shell *Shell) int {
+	cmdName := c.Args[0]
+	switch cmdName {
+	case "echo":
+		fmt.Println(strings.Join(c.Args[1:], " "))
+		return 0
+	case "type":
+		arg := c.Args[1]
+		if Builtins[arg] {
+			fmt.Printf("%s is a shell builtin\n", arg)
+		} else if path := findInPath(arg); path != "" {
+			fmt.Printf("%s is %s\n", arg, path)
+		} else {
+			fmt.Printf("%s: not found\n", arg)
+		}
+		return 0
+	}
+	return 1
+}
+
+func (c *Command) executeExternal() int {
+	cmdName := c.Args[0]
+
+	cmdPath := findInPath(cmdName)
+	if cmdPath == "" {
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", cmdName)
+		return 127
+	}
+
+	cmd := exec.Command(cmdPath, c.Args[1:]...)
+	cmd.Args = c.Args
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	for _, redir := range c.Redirections {
+		switch redir.Type {
+		case TokenRedirectOut:
+			f, err := os.Create(redir.Filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %s: %s\n", cmdName, redir.Filename, err)
+				return 1
 			}
-		default:
-			fmt.Println(command + ": command not found")
+			defer f.Close()
+			cmd.Stdout = f
+
+		case TokenRedirectAppend:
+			f, err := os.OpenFile(redir.Filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %s: %s\n", cmdName, redir.Filename, err)
+				return 1
+			}
+			defer f.Close()
+			cmd.Stdout = f
+
+		case TokenRedirectIn:
+			f, err := os.Open(redir.Filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %s: No such file or directory\n", cmdName, redir.Filename)
+				return 1
+			}
+			defer f.Close()
+			cmd.Stdin = f
+
+		case TokenRedirect2:
+			f, err := os.Create(redir.Filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %s: %s\n", cmdName, redir.Filename, err)
+				return 1
+			}
+			defer f.Close()
+			cmd.Stderr = f
 		}
 	}
+
+	err := cmd.Run()
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			return exitErr.ExitCode()
+		}
+		return 1
+	}
+
+	return 0
+}
+
+func findInPath(cmdName string) string {
+	if strings.Contains(cmdName, "/") || strings.Contains(cmdName, "\\") {
+		if isExecutable(cmdName) {
+			return cmdName
+		}
+		return ""
+	}
+
+	pathEnv := os.Getenv("PATH")
+	paths := filepath.SplitList(pathEnv)
+
+	for _, dir := range paths {
+		fullPath := filepath.Join(dir, cmdName)
+
+		if isExecutable(fullPath) {
+			return fullPath
+		}
+
+		if fullPath+".exe" != fullPath {
+			if isExecutable(fullPath + ".exe") {
+				return fullPath + ".exe"
+			}
+		}
+	}
+
+	return ""
+}
+
+func isExecutable(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+
+	// Check if it's a regular file
+	if !info.Mode().IsRegular() {
+		return false
+	}
+
+	if info.Mode().Perm()&0111 != 0 {
+		return true
+	}
+
+	return false
 }
